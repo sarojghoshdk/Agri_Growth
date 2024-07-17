3b1e03073c1728e7e19454062254210068e8f6f1
Saroj Ghosh <115480098+sarojghoshdk@users.noreply.github.com>
2024-07-17 15:46:24 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,240 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+from utils.fertilizer import fertilizer_dic
+import requests
+import config
+import pickle
+#import markups
+from markupsafe import Markup
+
+# Module & Requirements for ChatBot
+ 
+import nltk
+nltk.download('popular')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+model = load_model('model.h5')
+import json
+import random
+intents = json.loads(open('data.json').read())
+words = pickle.load(open('texts.pkl','rb'))
+classes = pickle.load(open('labels.pkl','rb'))
+
+# ===========================================================
+
+# Loading crop recommendation model
+
+crop_recommendation_model_path = r'C:\Users\saroj\OneDrive\Desktop\Agri_growth_demo\Model\RandomForest.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
+
+
+# Custom functions for calculations
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+    
+
+# ===============================================================================================
+# ------------------------------------ FLASK APP -------------------------------------------------
+
+
+app = Flask(__name__)
+
+# render home page
+
+
+@ app.route('/')
+def home():
+    title = 'Agri_Growth - Home'
+    return render_template('index.html', title=title)
+
+# render crop recommendation form page
+
+
+@ app.route('/crop-recommend')
+def crop_recommend():
+    title = 'Agri_Growth - Crop Recommendation'
+    return render_template('crop.html', title=title)
+
+# render fertilizer recommendation form page
+
+
+@ app.route('/fertilizer')
+def fertilizer_recommendation():
+    title = 'Agri_Growth - Fertilizer Suggestion'
+
+    return render_template('fertilizer.html', title=title)
+
+
+# RENDER PREDICTION PAGES
+
+# render crop recommendation result page
+
+
+@ app.route('/crop-predict', methods=['POST'])
+def crop_prediction():
+    title = 'Agri_Growth - Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+
+        # state = request.form.get("stt")
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+
+            return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+        else:
+
+            return render_template('try_again.html', title=title)
+
+
+# render fertilizer recommendation result page
+
+
+@ app.route('/fertilizer-predict', methods=['POST'])
+def fert_recommend():
+    title = 'Agri_Growth - Fertilizer Suggestion'
+
+    crop_name = str(request.form['cropname'])
+    N = int(request.form['nitrogen'])
+    P = int(request.form['phosphorous'])
+    K = int(request.form['pottasium'])
+    # ph = float(request.form['ph'])
+
+    df = pd.read_csv('Data/fertilizer.csv')
+
+    nr = df[df['Crop'] == crop_name]['N'].iloc[0]
+    pr = df[df['Crop'] == crop_name]['P'].iloc[0]
+    kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+
+    n = nr - N
+    p = pr - P
+    k = kr - K
+    temp = {abs(n): "N", abs(p): "P", abs(k): "K"}
+    max_value = temp[max(temp.keys())]
+    if max_value == "N":
+        if n < 0:
+            key = 'NHigh'
+        else:
+            key = "Nlow"
+    elif max_value == "P":
+        if p < 0:
+            key = 'PHigh'
+        else:
+            key = "Plow"
+    else:
+        if k < 0:
+            key = 'KHigh'
+        else:
+            key = "Klow"
+
+    response = Markup(str(fertilizer_dic[key]))
+
+    return render_template('fertilizer-result.html', recommendation=response, title=title)
+
+# Code for chatbot page ===============================================
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+
+#from flask import Flask, render_template, request
+
+#app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/chatbot")
+def chatbot():
+    return render_template("chatbot.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
+
+# ==============================================================
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,1 @@
+weather_api_key = "9ed7da1be4cbd98eddff8060d3f85561"
